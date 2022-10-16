fee4c450a47fa26636d19cbd47f8f125065c50c8
bpinheiromg <bpinheiromg@gmail.com>
2022-10-16 12:37:03 -0300

    Section 9: Spring Configuration with Java Annotations - Bean Scopes and Lifecycle Methods

--- 02-spring-code-annotations/src/bpinheiromg/spring/demo/fortune/FileFortuneService.java
+++ 02-spring-code-annotations/src/bpinheiromg/spring/demo/fortune/FileFortuneService.java
@@ -1,33 +1,58 @@
 package bpinheiromg.spring.demo.fortune;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileFortuneService implements FortuneService {
 	
-	@Value("${fortune.01}")
-	private String fortune01;
-	
-	@Value("${fortune.02}")
-	private String fortune02;
-	
-	@Value("${fortune.03}")
-	private String fortune03;
 
-	private ArrayList<String> fortune;
+	private List<String> fortuneList;
 	
 	private Random rand = new Random();
 
-	public String getFortune() {
-		if (fortune==null) 
-			fortune = new ArrayList<>(Arrays.asList(fortune01,fortune02,fortune03));
-		
-		return fortune.get(rand.nextInt(fortune.size()));
+	@PostConstruct
+	private void setupFortune() {
+		System.out.println("Inital fortune: "+getFortune());		
 	}
+	
+	public String getFortune() {
+		fortuneListFromFile();
+		return fortuneList.get(rand.nextInt(fortuneList.size()));
+	}
+	
+	private void fortuneListFromFile() {
+		if (fortuneList==null) {
+			try {
+				fortuneList = Files.readAllLines(Paths.get("./resources/fortune.txt"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+//	@Value("${fortune.01}")
+//	private String fortune01;
+//	
+//	@Value("${fortune.02}")
+//	private String fortune02;
+//	
+//	@Value("${fortune.03}")
+//	private String fortune03;
+//
+//	public String getFortune() {
+//		if (fortune==null) 
+//			fortune = new ArrayList<>(Arrays.asList(fortune01,fortune02,fortune03));
+//		
+//		return fortune.get(rand.nextInt(fortune.size()));
+//	}
 
 }
