4e5249840e729ac63c10ed6d295e8b80e9e75f96
bpinheiromg <bpinheiromg@gmail.com>
2022-11-06 15:03:51 -0300

    Section 15: Spring MVC Form Validation - Applying Built-In Validation Rules (InitBinder to trim Strings)

--- 03-spring-mvc-basic-xml/src/bpinheiromg/spring/demo/mvc/controller/V6CustumerController.java
+++ 03-spring-mvc-basic-xml/src/bpinheiromg/spring/demo/mvc/controller/V6CustumerController.java
@@ -2,9 +2,12 @@ package bpinheiromg.spring.demo.mvc.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,13 @@ import bpinheiromg.spring.demo.mvc.model.V6CustomerValidationModel;
 @RequestMapping("/v6/customer")
 public class V6CustumerController {
 	
+	/** Automatically trim all Strings passed as parameter whenever a request is made */
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
 	@RequestMapping("/form")
 	public String showForm(Model model) {
 		
@@ -22,7 +32,7 @@ public class V6CustumerController {
 		return "v6-customer-form";		
 	}
 	
-	@RequestMapping("processForm")
+	@RequestMapping("/processForm")
 	public String procesForm(
 			@Valid @ModelAttribute("customer") V6CustomerValidationModel customer, BindingResult bindingResult) {
 		
